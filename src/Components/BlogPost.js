9dd8f8b7c393d73fe01ee01482db9cb8f181c36f
Rachel Campbell <rach.eliza.campbell@icloud.com>
2021-02-15 08:46:31 -0500

    code with build

---
+++ src/Components/BlogPost.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import Butter from 'buttercms'
+import { Helmet } from "react-helmet";
+
+const butter = Butter('f97fe920e02ef96c54289b5b72ee04d1a8821449');
+
+class BlogPost extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loaded: false
+    };
+  }
+
+  componentWillMount() {
+    let slug = this.props.params.slug;
+
+    butter.post.retrieve(slug).then((resp) => {
+      this.setState({
+        loaded: true,
+        post: resp.data.data
+      })
+    });
+  }
+
+  render() {
+    if (this.state.loaded) {
+      const post = this.state.post;
+
+      return (
+        <div>
+          <Helmet>
+            <title>{post.seo_title}</title>
+            <meta name="description" content={post.meta_description} />
+            <meta name="og:image" content={post.featured_image} />
+          </Helmet>
+
+          <h1>{post.title}</h1>
+          <div dangerouslySetInnerHTML={{__html: post.body}} />
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          Loading...
+        </div>
+      );
+    }
+  }
+}
+
+export default BlogPost;
